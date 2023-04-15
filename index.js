4ecf899b2f09926eb9468fe96ea6c5443731ce09
bijoymamud <bijoymamud.09@gmail.com>
2023-04-15 14:00:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+
+    res.send('My phone information')
+
+})
+
+app.listen(port, () => {
+    console.log(`My phone server is running on port: ${port}`);
+})
